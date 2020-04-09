281d7d0049664e179110e771b59a08f7b9fbe9a1
kaizhao1123 <zhaokai11231212@gmail.com>
2020-04-09 10:36:48 -0500

    update climate panel
    change the way to show the components, make it easer to read.

--- test3/src/test3/ClimatePanel.java
+++ test3/src/test3/ClimatePanel.java
@@ -71,7 +71,10 @@ public class ClimatePanel extends JPanel{
 	JLabel label;
 	JTable databaseTable;
 	JTable customTable;
-	JScrollPane scrollPane;
+	JScrollPane scrollPane;	
+	JPanel secondLeft;
+	JPanel locationPanel;
+	JPanel customPanel;
 	
 	// the values in different textfields	
 	JTextField valueOfPre;
@@ -124,10 +127,10 @@ public class ClimatePanel extends JPanel{
 		
     	/***k***
 		 * Set up the layout and add components into it
+		 * Split the screen into two parts: 	A. firstLine(north), B.secondPart(center)
 		 ***z***/								
-		BorderLayout border= new BorderLayout();  //split the screen into two parts: 	A. firstLine(north), B.secondPart(center)
+		BorderLayout border= new BorderLayout(); 
 		this.setLayout(border);
-
 		
 		// build actionlisterner for button to show whether the action works or not					
 		ActionListener sliceActionListener = new ActionListener() {
@@ -146,10 +149,8 @@ public class ClimatePanel extends JPanel{
 		GridBagLayout firstLineLayOut = new GridBagLayout();
 		GridBagConstraints gbc1 =  new GridBagConstraints();		
 		firstPart.setLayout(firstLineLayOut);
-		
-		//gbc1.insets = new Insets(0,1,0,3);	
 
-		//// A.1 : the panel for "Select Climate Data Source"	
+		// A.1 : the panel for "Select Climate Data Source"	
 		gbc1.gridx = 0;
 		gbc1.gridy = 1;
 		gbc1.fill = GridBagConstraints.BOTH;
@@ -172,15 +173,16 @@ public class ClimatePanel extends JPanel{
 					valueOfOCV.setText(currentElement.data[27]);
 					valueOfLRV.setText(currentElement.data[28]);
 					valueOfAna.setText(currentElement.data[26]);					
-					scrollPane.setViewportView(databaseTable);									
+					scrollPane.setViewportView(databaseTable);
+					secondLeft.remove(customPanel);
+					secondLeft.add(locationPanel,0);
+					secondLeft.updateUI();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});
-		
-		//r2.addActionListener(sliceActionListener);
 		r2.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e){
@@ -191,18 +193,20 @@ public class ClimatePanel extends JPanel{
 					valueOfOCV.setText("0");
 					valueOfLRV.setText("0");
 					valueOfAna.setText("0");
-					scrollPane.setViewportView(customTable);		        
+					scrollPane.setViewportView(customTable);
+					secondLeft.remove(locationPanel);
+					secondLeft.add(customPanel,0);
+					secondLeft.updateUI();
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});		
-		scds.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Select Climate Data Source"));
-		
+		scds.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Select Climate Data Source"));		
 		firstPart.add(scds, gbc1);  // add the panel into the firstpart panel
 
-		//// A.2 :  the panel for "Options for Evaluating Monthly Net Prec - Evap"	
+		// A.2 :  the panel for "Options for Evaluating Monthly Net Prec - Evap"	
 		gbc1.gridx = 1;
 		gbc1.gridy = 1;
 		gbc1.gridwidth = 2;
@@ -226,44 +230,30 @@ public class ClimatePanel extends JPanel{
 		
 	
 		/***k***
-		 * B. build the second part, which including two child panel: secondLeft and secondRight. The "secondLeft" includes "location", "other", "lagoon loading rates",
+		 * B. Build the second part, which including two child panel: secondLeft and secondRight. 
+		 * 	  The "secondLeft" includes panel("location"), panel("Precipitation"), label("lagoon loading rates"), panel(RDM), panel(NDM);
 		 *    and the "secondRight" includes "table" and "buttons".
 		 ***z***/
 		
 		
 		// build the main panel of the second part
-		JPanel secondPart = new JPanel();   
-		GridBagLayout secondPartLayOut = new GridBagLayout();
-		GridBagConstraints gbcTwo =  new GridBagConstraints();		
-		secondPart.setLayout(secondPartLayOut);
+		JPanel secondPart = new JPanel(); 
+		secondPart.setLayout(new GridBagLayout());
+		GridBagConstraints gbcTwo =  new GridBagConstraints();	
 		
-		//// B.L build the panel: secondLeft, including  "location", including two child panel "county" and "station"
-		
+		//B.L build the panel: secondLeft		
 		gbcTwo.gridx = 0;
 		gbcTwo.gridy = 0;
 		gbcTwo.fill = GridBagConstraints.BOTH;
-		
-		
-		JPanel secondLeft = new JPanel();   
-		GridBagLayout secondLeftLayOut = new GridBagLayout();
-		GridBagConstraints gbcTwoLeft =  new GridBagConstraints();		
-		secondLeft.setLayout(secondLeftLayOut);
-		
-		// B.L.1 : the panel for "location"
-		gbcTwoLeft.gridx = 0;
-		gbcTwoLeft.gridy = 0;
-		gbcTwoLeft.fill = GridBagConstraints.BOTH;
-
-		JPanel location = new JPanel();
-		GridLayout locLayOut = new GridLayout(2,1);
-		location.setLayout(locLayOut);
-		
-		// B.L.1.1 :the panel for "county"
-		JPanel couPanel = new JPanel();
-		FlowLayout flo1 = new FlowLayout();
-		couPanel.setLayout(flo1);
-		JLabel county = new JLabel("Select County:");
 				
+		
+		secondLeft = new JPanel();   
+		secondLeft.setLayout(new GridBagLayout());
+		GridBagConstraints gbcTwoLeft =  new GridBagConstraints();		
+				
+		// B.L.1 : the panel for "selecting location"
+		
+		locationPanel = new JPanel();		
 		String[] listOfCounty = new String[countyStationMap.size()];				
 		Set<String> keySet = countyStationMap.keySet();
 		int count = 0;
@@ -271,10 +261,9 @@ public class ClimatePanel extends JPanel{
 			listOfCounty[count] = s;
 			count++;
 		}				
-		Arrays.sort(listOfCounty);	
+		Arrays.sort(listOfCounty);		
 		
-		
-		
+		JLabel labelCounty = new JLabel("Select County:");		
 		bCounty = new JComboBox(listOfCounty);				
 		bCounty.setPreferredSize(new Dimension(230,25));
 		//bCounty.setSelectedIndex(0);
@@ -306,18 +295,11 @@ public class ClimatePanel extends JPanel{
 						}
 					}
 				});
-		couPanel.add(county);couPanel.add(bCounty);
-		
-				
-		// B.L.1.2 : the panel for "station"
-		JPanel staPanel = new JPanel();
-		FlowLayout flo2 = new FlowLayout();
-		couPanel.setLayout(flo2);
-		JLabel station = new JLabel("Select Station:");
-		
+	
 		ArrayList<String> firstCounty = countyStationMap.get(listOfCounty[0]);
-		String[] listOfStation = convertToStringList(firstCounty);			
-				
+		String[] listOfStation = convertToStringList(firstCounty);	
+		
+		JLabel labelStation = new JLabel("Select Station:");		
 		DefaultComboBoxModel stationModel = new DefaultComboBoxModel( listOfStation );		
 		bStation.setModel(stationModel);	
 		bStation.setPreferredSize(new Dimension(230,25));
@@ -334,93 +316,134 @@ public class ClimatePanel extends JPanel{
 						}
 					}
 				});
-		staPanel.add(station);staPanel.add(bStation);
-		location.add(couPanel);location.add(staPanel);
-		secondLeft.add(location,gbcTwoLeft);   // add "location" to second left.
-			
-		
-		//// B.L.2 : the panel for ***other***
-		gbcTwoLeft.fill = GridBagConstraints.HORIZONTAL;     
-		gbcTwoLeft.gridx = 0;
-		gbcTwoLeft.gridy = 1;
-        
-        JPanel other = new JPanel();
+		locationPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gcl = new GridBagConstraints();		
+		gcl.anchor = GridBagConstraints.NORTHWEST;
+		gcl.insets = new Insets(5,10,0,10);
+		gcl.gridx = 0;
+		gcl.gridy = 0;
+		locationPanel.add(labelCounty,gcl);
+		gcl.gridx = 1;
+		locationPanel.add(bCounty,gcl);
+		gcl.gridx = 0;
+		gcl.gridy = 1;
+		locationPanel.add(labelStation,gcl);
+		gcl.gridx = 1;
+		locationPanel.add(bStation,gcl);
+					
+		//// B.L.2 : the panel for "Precipitation"  
+       
+        JPanel prePanel = new JPanel();
 		JLabel precipitation = new JLabel("25 Yr. -24 Hr. Storm Precipitation:");
 		valueOfPre.setPreferredSize(new Dimension(60,25));
 		JLabel inches = new JLabel("inches");		
-		other.add(precipitation);other.add(valueOfPre);other.add(inches);	
-		secondLeft.add(other, gbcTwoLeft);  // add "other" to second left.
-        
+		prePanel.add(precipitation);prePanel.add(valueOfPre);prePanel.add(inches);	
+      
+					
+		// B.L.3 : the label
+		JLabel labelRate = new JLabel("Lagoon Loading Rates:");
 		
-		//// B.L.3 : the panel for "Lagoon Loading Rates:, including three child panels: title, "rational design method" and "NRCS Design Method"
-
-		gbcTwoLeft.gridx = 0;
-		gbcTwoLeft.gridy = 2;		
-             
-		JPanel lagoon = new JPanel();
-		lagoon.setLayout(new GridBagLayout());
-		GridBagConstraints gbcOfLag = new GridBagConstraints();
-		gbcOfLag.gridwidth = GridBagConstraints.REMAINDER;
-		gbcOfLag.fill = GridBagConstraints.HORIZONTAL;
-		
-		// B.L.3.1 : title
-		JLabel title = new JLabel("Lagoon Loading Rates:");
-		lagoon.add(title, gbcOfLag);
+		// B.L.4 : the panel for "rational design method" 
 			
-		// B.L.3.2 : the panel for "rational design method", including three small parts: "KVAL", "OCV" and "LRV"
-		gbcOfLag.insets= new Insets(5,0,0,0);
-		JPanel rational = new JPanel();			
-		rational.setLayout(new GridLayout(3,1));
+		JPanel rationalPanel = new JPanel();		
 		
-		// B.L.3.2.1 : the panel for "KVAL"
-		JPanel barthKVAL = new JPanel();
-		FlowLayout flo3 = new FlowLayout(FlowLayout.LEFT);
-		barthKVAL.setLayout(flo3);		
-		JLabel kval = new JLabel("Barth KVAL:");
+		JLabel labelKval = new JLabel("Barth KVAL:");
+		JLabel labelOcv = new JLabel("Load Rate for Odor,OCV:");
+		JLabel labelLrv = new JLabel("LRV Max:");
 		valueOfKVAL.setPreferredSize(new Dimension(65,25));
-		barthKVAL.add(kval);
-		barthKVAL.add(Box.createHorizontalStrut(65));
-		barthKVAL.add(valueOfKVAL);
-		rational.add(barthKVAL);
-		
-		//B.L.3.2.2 : the panel for "OCV"
-		JPanel loadRateForOCV = new JPanel();
-		FlowLayout flo4 = new FlowLayout(FlowLayout.LEFT);
-		loadRateForOCV.setLayout(flo4);	
-		JLabel ocv = new JLabel("Load Rate for Odor,OCV:");
 		valueOfOCV.setPreferredSize(new Dimension(65,25));
-		JLabel jl1 = new JLabel("lbx VS/cu. ft/day");
-		loadRateForOCV.add(ocv); loadRateForOCV.add(valueOfOCV); loadRateForOCV.add(jl1);
-		rational.add(loadRateForOCV);
-		
-		// B.L.3.2.3 : the panel for "LRV"
-		JPanel LRVMax = new JPanel();
-		FlowLayout flo5 = new FlowLayout(FlowLayout.LEFT);
-		LRVMax.setLayout(flo5);	
-		JLabel lrv = new JLabel("LRV Max:");
 		valueOfLRV.setPreferredSize(new Dimension(65,25));
+		JLabel jl1 = new JLabel("lbx VS/cu. ft/day");
 		JLabel jl2 = new JLabel("lbx VS/cu. ft/day");
-		LRVMax.add(lrv); 
-		LRVMax.add(Box.createHorizontalStrut(80));
-		LRVMax.add(valueOfLRV); LRVMax.add(jl2);
-		rational.add(LRVMax);		
 		
-		rational.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Rational Design Method"));		
-        lagoon.add(rational, gbcOfLag);
+		rationalPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbcRa = new GridBagConstraints();
+		gbcRa.fill = GridBagConstraints.NORTHWEST;
+		gbcRa.insets= new Insets(5,5,0,0);
 		
-        // B.L.3.3 : the panel for "NRCS design method"
-		JPanel nrcs = new JPanel();
+		gbcRa.gridx = 0;
+		gbcRa.gridy = 0;
+		gbcRa.gridwidth = 2;
+		rationalPanel.add(labelKval, gbcRa);
+		gbcRa.gridy = 1;
+		gbcRa.gridwidth = 3;
+		rationalPanel.add(labelOcv,gbcRa);
+		gbcRa.gridwidth = 1;
+		gbcRa.gridy = 2;
+		rationalPanel.add(labelLrv, gbcRa);
+		
+		gbcRa.gridx = 3;
+		gbcRa.gridy = 0;
+		rationalPanel.add(valueOfKVAL,gbcRa);
+		gbcRa.gridy = 1;
+		rationalPanel.add(valueOfOCV,gbcRa);
+		gbcRa.gridy = 2;
+		rationalPanel.add(valueOfLRV,gbcRa);
+		
+		gbcRa.gridx = 4;
+		gbcRa.gridy = 1;
+		rationalPanel.add(jl1,gbcRa);
+		gbcRa.gridy = 2;
+		rationalPanel.add(jl2,gbcRa);
+			
+		rationalPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Rational Design Method"));		
+		rationalPanel.setPreferredSize(new Dimension(355,120));
+		
+	    // B.L.5 : the panel for for "NRCS design method"
+		JPanel nrcsPanel = new JPanel();
 		JLabel alr = new JLabel("Anaerobic Load Rate:");
 		JLabel jl3 = new JLabel("lbs VS/1000 cu.ft/day");		
 		valueOfAna.setPreferredSize(new Dimension(65,25));
-		nrcs.add(alr);
-		nrcs.add(Box.createHorizontalStrut(10));
-		nrcs.add(valueOfAna);nrcs.add(jl3);
-		nrcs.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"NRCS Design Method"));
-        lagoon.add(nrcs, gbcOfLag);
+		nrcsPanel.add(alr);
+		nrcsPanel.add(Box.createHorizontalStrut(10));
+		nrcsPanel.add(valueOfAna);nrcsPanel.add(jl3);
+		nrcsPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"NRCS Design Method"));
+        
 		
-        secondLeft.add(lagoon,gbcTwoLeft);  // add "lagoon" into the second left.
+		
+		// setup for layout of left
+		gbcTwoLeft.anchor = GridBagConstraints.NORTHWEST;
+		gbcTwoLeft.insets = new Insets(0,10,0,10);
+		
+		gbcTwoLeft.gridx = 0;
+		gbcTwoLeft.gridy = 0;
+		secondLeft.add(locationPanel,gbcTwoLeft);
+		gbcTwoLeft.gridy = 1;
+		secondLeft.add(prePanel,gbcTwoLeft);
+		gbcTwoLeft.gridy = 2;
+		secondLeft.add(labelRate,gbcTwoLeft);
+		gbcTwoLeft.gridy = 3;
+		secondLeft.add(rationalPanel,gbcTwoLeft);
+		gbcTwoLeft.gridy = 4;
+		secondLeft.add(nrcsPanel,gbcTwoLeft);				
         secondPart.add(secondLeft,gbcTwo);  // add second left into the second part.
+        
+        // the panel of "Entering location" for customer
+        
+        customPanel = new JPanel();
+        JLabel labelEnterCounty = new JLabel("Enter County: ");
+        JLabel labelEnterStation = new JLabel("Enter Station: ");
+        JTextField textEnterCounty = new JTextField();
+        textEnterCounty.setPreferredSize(new Dimension(230,25));
+        JTextField textEnterStation = new JTextField();
+        textEnterStation.setPreferredSize(new Dimension(230,25));
+        
+        customPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gcEnter = new GridBagConstraints();		
+		gcEnter.anchor = GridBagConstraints.NORTHWEST;
+		gcEnter.insets = new Insets(5,10,0,10);
+		gcEnter.gridx = 0;
+		gcEnter.gridy = 0;
+		customPanel.add(labelEnterCounty,gcEnter);
+		gcEnter.gridx = 1;
+		customPanel.add(textEnterCounty,gcEnter);
+		gcEnter.gridx = 0;
+		gcEnter.gridy = 1;
+		customPanel.add(labelEnterStation,gcEnter);
+		gcEnter.gridx = 1;
+		customPanel.add(textEnterStation,gcEnter);
+        
+        
 
 		// B.R : the panel secondRight, including "table" and "buttons".
         		
